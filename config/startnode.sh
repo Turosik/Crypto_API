a6c8222929c62a05227fd831c306bf371f926233
turosik <turosik@protonmail.com>
2021-04-04 23:59:26 +0300

    increase treads

--- config/startnode.sh
+++ config/startnode.sh
@@ -1,1 +1,1 @@
-geth --networkid 151515 --mine --miner.threads=1 --miner.gasprice=1000000 --datadir "/home/turosik/ETH" --rpc --rpcport "8545" --rpcapi eth,web3,personal,net,miner --ipcpath "/home/turosik/ETH/geth.ipc"
+geth --networkid 151515 --mine --miner.threads=2 --miner.gasprice=1000000 --datadir "/home/turosik/ETH" --rpc --rpcport "8545" --rpcapi eth,web3,personal,net,miner --ipcpath "/home/turosik/ETH/geth.ipc"
